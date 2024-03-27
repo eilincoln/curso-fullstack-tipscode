e3673a9c3aedc66aef3f86d15ba9509b43fb03a2
eilincoln <lincoln.santospro@gmail.com>
2024-03-27 10:13:06 -0300

    adicionando módulo de javascript e alguns exercicios

---
+++ javascript/aula-02/index.js
@@ -0,0 +1,6 @@
+let meuNome = 'Lincoln'
+let seuNome = 'Kamilly'
+
+
+console.log(meuNome)
+console.log(seuNome)
---
+++ javascript/aula-03/index.js
@@ -0,0 +1,19 @@
+// aula sobre a lenght
+
+// let nome = 'Elias';
+
+// console.log(nome.length);
+
+// let caracter = 0;
+// let usuarioDigitouRecebido = prompt('Escreva alguma coisa');
+// let quantidade = usuarioDigitouRecebido.length;
+// let sobrouCaracter = 140 - quantidade;
+
+// console.log('O usuário digitou:  ' + usuarioDigitouRecebido + ' Faltam: ' + sobrouCaracter);
+
+
+// Exercicio!
+let twitter = prompt('Escreva alguma coisa: ');
+let limiteDe140 = twitter.slice(0, 140);
+
+alert(limiteDe140);
