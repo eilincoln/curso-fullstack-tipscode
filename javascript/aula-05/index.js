7a12899820e03ad284edca86297fecaf63f622d9
eilincoln <lincoln.santospro@gmail.com>
2024-03-27 18:37:15 -0300

    adicionando aula 5

---
+++ javascript/aula-05/index.js
@@ -0,0 +1,9 @@
+/*
+    Exercicio idade do cachorro.
+*/
+
+let idadeCachorro = prompt("Digite a idade do cachorro: ");
+
+let idadeHumano = (idadeCachorro -2) * 4 + 21;
+
+alert("Idade para humano: " + idadeHumano)
