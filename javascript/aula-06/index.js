26fc8e8a3d582e1a329eafdd001b5c707d7c61dd
eilincoln <lincoln.santospro@gmail.com>
2024-03-27 20:42:17 -0300

    atualição

---
+++ javascript/aula-06/index.js
@@ -0,0 +1,13 @@
+function getPao(pao) {
+    console.log("SairDeCasa");
+    console.log("MoveDireita");
+    console.log("MoveDireita");
+    console.log("MoveCima");
+    console.log("MoveCima");
+    console.log("MoveCima");
+    console.log("MoveCima");
+    let valorPao = Math.floor(pao / 1.5);
+    console.log("O robo vai comprar " + valorPao + " no mercado")
+}
+
+getPao(1.5)
