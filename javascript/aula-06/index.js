29591fc08a6238c062dfe06548d18c223f51ed5e
eilincoln <lincoln.santospro@gmail.com>
2024-03-27 21:10:27 -0300

    resolução do exerc

--- javascript/aula-06/index.js
+++ javascript/aula-06/index.js
@@ -1,13 +1,27 @@
-function getPao(pao) {
-    console.log("SairDeCasa");
-    console.log("MoveDireita");
-    console.log("MoveDireita");
-    console.log("MoveCima");
-    console.log("MoveCima");
-    console.log("MoveCima");
-    console.log("MoveCima");
-    let valorPao = Math.floor(pao / 1.5);
-    console.log("O robo vai comprar " + valorPao + " no mercado")
+// function getPao(pao) {
+//     console.log("SairDeCasa");
+//     console.log("MoveDireita");
+//     console.log("MoveDireita");
+//     console.log("MoveCima");
+//     console.log("MoveCima");
+//     console.log("MoveCima");
+//     console.log("MoveCima");
+//     let valorPao = Math.floor(pao / 1.5);
+//     console.log("O robo vai comprar " + valorPao + " no mercado")
+// }
+
+// getPao(1.5)
+
+// console.log();
+
+
+function lifeInWeeks(age) {
+    let yearsRemaining = 90 - age;
+    let days = yearsRemaining * 365;
+    let weeks = yearsRemaining * 52;
+    let months = yearsRemaining * 12;
+
+    console.log('Você tem ' + days + "dias " + weeks + " semanas " + months + "meses restantes")
 }
 
-getPao(1.5)
+lifeInWeeks(20);
