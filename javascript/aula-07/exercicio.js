1b15ad072a921b01af92c6895a646a5033a9ab6f
eilincoln <lincoln.santospro@gmail.com>
2024-03-28 10:01:12 -0300

    adicionando resolução do exercicio imc

--- javascript/aula-07/exercicio.js
+++ javascript/aula-07/exercicio.js
@@ -1,3 +1,15 @@
-function imc() {
-    
+// aqui faço o passo a passo para poder entender o código.
+let peso = 85;
+let altura = 1.75;
+
+let imc = peso / (altura * altura);
+
+console.log(Math.round(imc*100)/100);
+
+//aqui já transformo o código na func para poder resolver o exercício
+function calcImc(peso, altura) {
+    let imc = peso / (altura * altura);
+    return console.log(Math.round(imc*100)/100);
 }
+
+calcImc(85, 1.75);
