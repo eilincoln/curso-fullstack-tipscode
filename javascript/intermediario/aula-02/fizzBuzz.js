896eb1b10dab8efe3213a4ce2961e52a54d4bd0c
eilincoln <lincoln.santospro@gmail.com>
2024-03-28 19:35:16 -0300

    finalização do exercicio anterior e add novo sequência de fibonacci

--- javascript/intermediario/aula-02/fizzBuzz.js
+++ javascript/intermediario/aula-02/fizzBuzz.js
@@ -1,21 +1,24 @@
 let output = []
 
-let count = 1
+
 
 function fizzBuz(){
-    if(count % 3 === 0 && count % 5 === 0){
-        output.push('FizzBuzz')
+    for(let count = 1; count <= 50; count++ ) {
+        if(count % 3 === 0 && count % 5 === 0){
+            output.push('FizzBuzz')
+        }
+    
+    
+        if (count % 3 === 0 ) {
+            output.push('Fizz')
+        } else if(count % 5 === 0) {
+            output.push('Buz')
+        } else {
+            output.push(count)
+        }
     }
-
-
-    if (count % 3 === 0 ) {
-        output.push('Fizz')
-    } else if(count % 5 === 0) {
-        output.push('Buz')
-    } else {
-        output.push(count)
-    }
-    count++
     console.log(output)
 }
+
+fizzBuz()
 
