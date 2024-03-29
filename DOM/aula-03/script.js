93985f3c2c02a12d9f204de727b10de88b12fc93
eilincoln <lincoln.santospro@gmail.com>
2024-03-29 15:36:49 -0300

    finalização da aula sobre o querrySelector

--- DOM/aula-03/script.js
+++ DOM/aula-03/script.js
@@ -1,25 +1,16 @@
+// Seleciona todos os elementos com a classe "minha-classe"
+const elementos = document.querySelectorAll('.minha-classe');
+
+// Itera sobre os elementos selecionados
+elementos.forEach(elemento => {
+    // Faz algo com cada elemento, por exemplo, altera seu texto
+    elemento.textContent = 'Texto alterado';
+});
+
 /*
-    aqui eu posso usar o query que vai me ajudar a pegar o seletor igual eu pego no css
+    buscar usar mais o querySelector/All para que eu possa usar sempre esse
 */
 
-const h1 = document.querySelector('h1')
-h1.style.color = 'red'
+document.querySelector('li a').style.color = 'red'
 
-// aqui eu posso pegar de forma mais especifica
-const maisEspec = document.querySelector('li a')
-
-// Selecionando um elemento pela sua classe
-const elemento = document.querySelector('.minha-classe');
-
-// Selecionando um elemento pelo seu ID
-const elemento2 = document.querySelector('#meu-id');
-
-// Selecionando um elemento pelo nome da tag
-const elemento3 = document.querySelector('div');
-
-// Selecionando um elemento pela sua classe dentro de outro elemento
-const elemento4 = document.querySelector('div .minha-classe');
-
-// Selecionando um elemento pelo seu ID dentro de outro elemento
-const elemento5 = document.querySelector('.outro-elemento #meu-id');
-
+document.querySelector('.terceira').style.color = 'yellow'
