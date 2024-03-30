9ed038933430f2c2e0893e4c7823fd0e485d1eac
eilincoln <lincoln.santospro@gmail.com>
2024-03-30 20:36:56 -0300

    adicionando sons e aprendendo um pouco mais de javascript

--- projetos/bateria-start/app.js
+++ projetos/bateria-start/app.js
@@ -2,7 +2,10 @@ let numberOfDrumButtons = document.querySelectorAll('.drum').length
 
 for(let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll('button')[i].addEventListener('click', function(){
-        alert("clicou")
+        console.log(this.innerHTML)
     })
 }
 
+
+// let audio = new Audio('./sounds/tom-1.mp3');
+// audio.play();       
