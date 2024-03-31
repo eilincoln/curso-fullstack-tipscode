b35c5ca3b12c2878e5035e89fa2f9934ab36de7b
eilincoln <lincoln.santospro@gmail.com>
2024-03-30 22:01:15 -0300

    adicionando atualização para a forma de pandeiro no teclado, com funcionalidade

--- projetos/bateria-start/app.js
+++ projetos/bateria-start/app.js
@@ -3,41 +3,46 @@ let numberOfDrumButtons = document.querySelectorAll('.drum').length
 for(let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll('button')[i].addEventListener('click', function(){
         let buttonInnerHTML = this.innerHTML
-        switch (buttonInnerHTML) {
-            case 'w':
-                let tom1 = new Audio('/sounds/tom-1.mp3');
-                tom1.play(); 
-                break;
-            case 'a':
-                let tom2 = new Audio('./sounds/tom-2.mp3');
-                tom2.play(); 
-            break;
-            case 's':
-                let tom3 = new Audio('./sounds/tom-4.mp3');
-                tom3.play(); 
-            break;
-            case 'd':
-                let tom = new Audio('./sounds/tom-1.mp3');
-                tom.play(); 
-            break;
-            case 'j':
-                let snare = new Audio('./sounds/snare.mp3');
-                snare.play(); 
-            break;
-            case 'k':
-                let crash = new Audio('./sounds/crash.mp3');
-                crash.play(); 
-            break;
-            case 'l':
-                let kick = new Audio('./sounds/kick-bass.mp3');
-                kick.play(); 
-            break;
-
-            default: console.log(buttonInnerHTML)
-        
-        }
+        makeSound(buttonInnerHTML)
     })
 }
 
-// let audio = new Audio('./sounds/tom-1.mp3');
-// audio.play();       
+document.addEventListener('keydown', function(event){
+    makeSound(event.key)
+})
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('/sounds/tom-1.mp3');
+            tom1.play(); 
+            break;
+        case 'a':
+            let tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play(); 
+        break;
+        case 's':
+            let tom3 = new Audio('./sounds/tom-4.mp3');
+            tom3.play(); 
+        break;
+        case 'd':
+            let tom = new Audio('./sounds/tom-1.mp3');
+            tom.play(); 
+        break;
+        case 'j':
+            let snare = new Audio('./sounds/snare.mp3');
+            snare.play(); 
+        break;
+        case 'k':
+            let crash = new Audio('./sounds/crash.mp3');
+            crash.play(); 
+        break;
+        case 'l':
+            let kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play(); 
+        break;
+
+        default: console.log(buttonInnerHTML)
+    
+    }
+}
